e454b65b0e238076e3f5cf297e62d4993527cc06
Tenzin <tenthinten@gmail.com>
2021-12-31 22:17:29 +0530

    create signin function

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def home(request):
@@ -32,6 +33,22 @@ def signup(request):
     return render(request, "authentication/signup.html")
 
 def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "authentication/index.html", {'fname':fname})
+
+        else:
+            messages.error(request, "Bad credentials")
+            return redirect('home')
+
     return render(request, "authentication/signin.html")
 
 def signout(request):
