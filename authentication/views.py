beb3e5794983b5d6f2f21a7658bc9afdb4dca425
Tenzin <tenthinten@gmail.com>
2021-12-31 19:45:23 +0530

    takes in all the information by user

--- authentication/views.py
+++ authentication/views.py
@@ -7,6 +7,19 @@ def home(request):
     return render(request, "authentication/index.html")
 
 def signup(request):
+
+    if request.method == "POST":
+        # username = request.POST.get('username')
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+
+
+
     return render(request, "authentication/signup.html")
 
 def signin(request):
